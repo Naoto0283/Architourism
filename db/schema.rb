19ee2bc3e34ac81834240238ef51729d16b8a9a0
naoto <naoto2023010@gmail.com>
2024-12-25 15:11:09 +0900

    ユーザー登録機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_24_062257) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_25_053240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,5 +18,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_24_062257) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end

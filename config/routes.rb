c3b8548331e04c9f2a9e0028662bc59c1ab285fc
naoto <naoto2023010@gmail.com>
2025-01-07 16:11:41 +0900

    場所検索機能設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   resources :tasks
+
+  #トップページのルーティング
   root "static_pages#top"
+
+  #ユーザー登録のルーティング
   resources :users, only: %i[new create]
+
+  #ログイン/ログアウト機能のルーティング
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
+
+  # 場所検索ページのルーティング
+  get 'spots/map'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

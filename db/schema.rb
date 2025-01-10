eb4c973dabc472c5b79bf56e7bc3d4f5c4c27c68
naoto <naoto2023010@gmail.com>
2025-01-10 21:27:55 +0900

    geocoger導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_10_120220) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_10_121454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,24 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_10_120220) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "spots", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "name", null: false
+    t.string "postal_code"
+    t.string "address"
+    t.string "phone_number"
+    t.string "opening_hours"
+    t.string "web_site"
+    t.decimal "rating"
+    t.decimal "latitude", precision: 10, scale: 7, null: false
+    t.decimal "longitude", precision: 10, scale: 7, null: false
+    t.string "place_id", null: false
+    t.string "photo_reference"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_spots_on_category_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -36,4 +54,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_10_120220) do
     t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "spots", "categories"
 end

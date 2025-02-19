0fb08f6ac4b2f35386ba9f497fb7d8c8287974af
naoto <naoto2023010@gmail.com>
2025-02-19 11:44:39 +0900

    Sorceryを使用したGoogle認証機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
 
   has_many :review, dependent: :destroy
 
+  # 外部サービス（GoogleやFacebookなど）の認証情報を Authentication モデルに保存するための関連付け
+  has_many :authentications, :dependent => :destroy
+  # ユーザー作成時に Authentication モデルの情報も同時に保存できるようにする
+  accepts_nested_attributes_for :authentications
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
